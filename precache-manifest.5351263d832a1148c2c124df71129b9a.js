2757e12db5d34ed728efb366ef5b5b78bcd0e7cd
jinredhub <jin@nieblo.com>
2020-03-01 08:21:34 -0800

    test react build

---
+++ precache-manifest.5351263d832a1148c2c124df71129b9a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44c4f51d7f547d7f423c9e4a3125e87f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7f4250cf78b1bdb9dfd",
+    "url": "/static/css/main.dbb54c53.chunk.css"
+  },
+  {
+    "revision": "b5446e6d129d4387b6af",
+    "url": "/static/js/2.5e09ae82.chunk.js"
+  },
+  {
+    "revision": "59b1ab6bf8d1c9609a9b8cc7f6e69f9d",
+    "url": "/static/js/2.5e09ae82.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7f4250cf78b1bdb9dfd",
+    "url": "/static/js/main.4df30496.chunk.js"
+  },
+  {
+    "revision": "71f88fae3c7f2c4cde1d",
+    "url": "/static/js/runtime-main.c511cfcb.js"
+  },
+  {
+    "revision": "d7f8d013ab0714751bf9662c78185c93",
+    "url": "/static/media/bg2.d7f8d013.png"
+  },
+  {
+    "revision": "1ae84bdb2eb2a43ef32f049e733981b7",
+    "url": "/static/media/logo.1ae84bdb.png"
+  }
+]);
